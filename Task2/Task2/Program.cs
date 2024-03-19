29de2444d40d43c6f54036509be1999b7de6608b
Ilia Bondarenko <iluharider2002@gmail.com>
2024-03-19 18:03:34 +0300

    task2 hw5

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StackList<T>
+{
+    private readonly int _maxStackSize;
+    private readonly List<Stack<T>> _stacks;
+
+    public StackList(int maxStackSize)
+    {
+        if (maxStackSize <= 0)
+        {
+            throw new ArgumentException("Max stack size must be greater than zero.");
+        }
+
+        _maxStackSize = maxStackSize;
+        _stacks = new List<Stack<T>>();
+    }
+
+    public void Push(T value)
+    {
+        if (_stacks.Count == 0 || _stacks[^1].Count == _maxStackSize)
+        {
+            _stacks.Add(new Stack<T>());
+        }
+
+        _stacks[^1].Push(value);
+    }
+
+    public T Pop()
+    {
+        if (_stacks.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+
+        T value = _stacks[^1].Pop();
+
+        if (_stacks[^1].Count == 0)
+        {
+            _stacks.RemoveAt(_stacks.Count - 1);
+        }
+
+        return value;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stackList = new StackList<int>(3);
+
+        stackList.Push(1);
+        stackList.Push(2);
+        stackList.Push(3);
+        stackList.Push(4); // here comes new stack
+
+        
+        Console.WriteLine(stackList.Pop()); 
+        Console.WriteLine(stackList.Pop()); 
+        Console.WriteLine(stackList.Pop()); 
+        Console.WriteLine(stackList.Pop()); 
+
+        try
+        {
+            Console.WriteLine(stackList.Pop()); 
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
